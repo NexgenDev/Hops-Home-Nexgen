00f5f06a262ea8f12f04b7c73a2c001e6a9c6be1
Royer G <royjuni3431@gmail.com>
2020-07-13 17:28:11 -0500

    Menu responsive y faq

---
+++ js/fbpexels.js
@@ -0,0 +1,8 @@
+window._mfq = window._mfq || [];
+(function () {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript";
+    mf.defer = true;
+    mf.src = "https://cdn.mouseflow.com/projects/53543b71-a94c-4e74-b3d6-2ee3ca0f7b64.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+})();
--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
+const navbar = document.getElementById('header')
+const menuLinks = document.getElementsByClassName('menu_link')
+const socialMediaIcons = document.getElementsByClassName('social-media__icons')
+const logo = document.getElementById('logo')
+const responsiveMenu = document.getElementById('menu_responsive')
+const menu = document.getElementsByClassName('header__menu')
+const menuItems = document.getElementsByClassName('menu__item')
+const menuIcon = document.getElementsByClassName('menu__responsive')
+
 window.onscroll = () => {
     let verticalScroll = window.scrollY
-    const navbar = document.getElementById('header')
-    const menuItems = document.getElementsByClassName('menu_link')
-    const socialMediaIcons = document.getElementsByClassName('social-media__icons')
-    const logo = document.getElementById('logo')
-    const responsiveMenu = document.getElementById('menu_responsive')
     if (verticalScroll >= 900) {
         // switch nabvar to purple
         navbar.classList.replace('header--white', 'header--purple')
-        for (let i = 0; i < menuItems.length; i++) {
-            menuItems[i].classList.add('menu_link--green')
+        for (let i = 0; i < menuLinks.length; i++) {
+            menuLinks[i].classList.add('menu_link--green')
         }
         for (let i = 0; i < socialMediaIcons.length; i++) {
             socialMediaIcons[i].classList.add('social-media__icons--green')
         }
-        logo.src='images/logo-hops-alternative.png'
-        responsiveMenu.classList.add('menu__responsive--white')
-        
+        logo.src = 'images/logo-hops-alternative.png'
     } else {
         // switch nabvar to white
         navbar.classList.replace('header--purple', 'header--white')
-        for (let i = 0; i < menuItems.length; i++) {
-            menuItems[i].classList.remove('menu_link--green')
+        for (let i = 0; i < menuLinks.length; i++) {
+            menuLinks[i].classList.remove('menu_link--green')
         }
         for (let i = 0; i < socialMediaIcons.length; i++) {
             socialMediaIcons[i].classList.remove('social-media__icons--green')
         }
-        logo.src='images/logo-hops.png'
-        responsiveMenu.classList.remove('menu__responsive--white')
+        logo.src = 'images/logo-hops.png'
     }
 }
 
@@ -40,3 +41,31 @@ const addYearToWeb = () => {
 const responsive = () => {}
 
 addYearToWeb()
+
+responsiveMenu.addEventListener('click', () => {
+    if (responsiveMenu.className == 'menu_responsive--show') {
+        // ocultar
+        const menu = document.getElementsByClassName('header__menu--show')
+        const menuItems = document.getElementsByClassName('menu__item--show')
+        const menuLinks = document.getElementsByClassName('menu_link--show')
+        menu[0].classList.add('header__menu')
+        menu[0].classList.remove('header__menu--show')
+        for (let i = 0; i < menuItems.length; i++) {
+            menuItems[i].classList.remove('menu__item--show')
+            menuLinks[i].classList.remove('menu_link--show')
+        }
+        
+        responsiveMenu.classList.remove('menu_responsive--show')
+        menuIcon[0].src = 'images/menu.svg'
+    } else {
+        // mostar
+        menu[0].classList.add('header__menu--show')
+        menu[0].classList.remove('header__menu')
+        for (let i = 0; i < menuItems.length; i++) {
+            menuItems[i].classList.add('menu__item--show')
+            menuLinks[i].classList.add('menu_link--show')
+        }
+        responsiveMenu.classList.add('menu_responsive--show')
+        menuIcon[0].src = 'images/x.svg'
+    }
+})
